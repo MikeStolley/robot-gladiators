e073f8b8b7040270c0e9c34d7befa2fef5b6aca0
Mike Stolley <stolleywilliam@yahoo.com>
2022-06-13 18:34:11 -0400

    added fight function and variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,47 @@
-window.alert("this is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName,playerAttack,playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+
+    enemyHealth = enemyHealth-playerAttack;
+
+    playerHealth = playerHealth - enemyAttack;
+
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
+    )
+
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died! ");
+    }
+
+    else {
+        window.alert(playerName + " still has " + " playerHealth" + " health left. ");
+    }
+
+    console.log (
+        playerName + " attacked " + enemyName + ". " + enemyName + " Now has " + enemyHealth + " health remaining. "
+    );
+
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died! ");
+    }
+    
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+    }
+    
+};
+
+
+
+fight();
